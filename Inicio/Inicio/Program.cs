d33eafd284ee8a03191179319f7968a0484e80df
marcelogr <celin@DESKTOP-N31CV6N>
2018-02-28 20:24:39 -0300

    Lendo e escrevendo o nome

---
+++ Inicio/Inicio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = string.Empty;
+
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Hello World" + nome);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
