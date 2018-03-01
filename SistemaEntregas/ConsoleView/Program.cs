b94b1a4eed71a147474d8d78f58fe278d76621e0
marcelogr <celin@DESKTOP-N31CV6N>
2018-02-28 22:20:21 -0300

    Classe definida como abstrata, objetos públicos.

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ConsoleView
+﻿using Modelos;
+
+namespace ConsoleView
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Pessoa p = new Pessoa(); //classe abstrata não pode ser chamada!
+            
         }
     }
 }
